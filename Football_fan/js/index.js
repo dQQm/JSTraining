4d51a0e39b823026998a94f055861fddf77cabfc
Unicorn <mariazhingova@gmail.com>
2016-06-26 19:54:21 +0300

    bamburger menu creation

--- Football_fan/js/index.js
+++ Football_fan/js/index.js
@@ -1,3 +1,12 @@
+'use strict';
 function menu(){
 	$('.menu').toggle();
+  
 }
+$('.toggle-nav').click(function(event) {
+  $(document).one('click',function() {
+    $('.menu').hide();
+  });
+
+  event.stopPropagation();
+});
